3b9ad45d13530a14a6374f8dd4a54f7c23293a2c
Simon Bugert <simon.bugert@googlemail.com>
2013-09-04 17:56:47 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    clean: {
+      dist: ['dist'],
+      init: ['src/*/vendor']
+    },
+
+    useminPrepare: {
+      options: {
+        dest: 'dist'
+      },
+      html: ['src/*.html']
+    },
+
+    cssmin: {
+    },
+
+    copy: {
+      main: {
+        files: [
+          {expand: true,
+           flatten: true,
+           src: ['src/*'],
+           dest: 'dist/',
+           filter: 'isFile'} // flattens results to a single level
+        ]
+      },
+      init: {
+        files: [
+          {expand: true, flatten: true, cwd: 'bower_components/', src: ['bootstrap/dist/js/bootstrap.js', 'jquery/jquery.js'], dest: 'src/js/vendor', filter: 'isFile'},
+          {expand: true, flatten: true, cwd: 'bower_components/', src: ['bootstrap/dist/css/bootstrap.css', 'bootstrap/dist/css/bootstrap-theme.css'], dest: 'src/css/vendor', filter: 'isFile'}
+        ]
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'dist',
+          src: '{,*/}*.html',
+          dest: 'dist'
+        }]
+      }
+    },
+
+    rev: {
+      files: {
+        src: ['dist/**/*.{js,css,png,jpg}']
+      }
+    },
+
+    usemin: {
+      html: ['dist/{,*/}*.html'],
+      css: ['dist/{,*/}*.css'],
+      options: {
+        dirs: ['dist']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-rev');
+
+  grunt.registerTask('default', ['clean:dist',
+                                 'copy:main',
+                                 'useminPrepare',
+                                 'concat',
+                                 'cssmin',
+                                 'uglify',
+                                 'rev',
+                                 'usemin']);
+  grunt.registerTask('init', ['clean:init', 'copy:init']);
+};
+
---
+++ server.js
@@ -0,0 +1,11 @@
+var st = require('st')
+var http = require('http')
+
+var mount = st({ path: __dirname + '/dist', url: '/', index: 'index.html'})
+http.createServer(function(req, res) {
+  var stHandled = mount(req, res);
+  if (stHandled)
+    return
+  else
+    res.end('this is not a static file')
+}).listen(8080)
